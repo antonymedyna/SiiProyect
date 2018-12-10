4c17cdffd19afc67e7bd4db9cd7e26c9b2086d99
antonymedyna <tony06191@hotmail.com>
2018-12-09 18:57:02 -0600

    First Commit

---
+++ SiiProyect/SiiProyect/Modelos/Inscripcion.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SiiProyect.Modelos
+{
+    class Inscripcion
+    {
+        public String materia { get; set; }
+    }
+}
